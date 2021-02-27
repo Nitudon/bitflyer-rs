f2deaa1bbaa65b75654a06088284b7f98005c6ed
udon <myudongen@gmail.com>
2021-02-28 02:09:18 +0900

    balance: 資産残高の取得APIの実装

---
+++ src/api/get_balance.rs
@@ -0,0 +1,39 @@
+use crate::api;
+use crate::api::ApiResponseError;
+
+const PATH : &'static str = "/v1/me/getbalance";
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum GetBalanceResponse {
+    Error { errors: Vec<String> },
+    Response(Vec<BalanceInfo>)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BalanceInfo {
+    pub currency_code: String,
+    pub amount: f32,
+    pub available: f32,
+}
+
+pub async fn get_balance() -> Result<Vec<BalanceInfo>, ApiResponseError> {
+    let response = api::get::<GetBalanceResponse>(&PATH).await?;
+    println!("{:?}", response);
+
+    match response {
+        GetBalanceResponse::Error { errors } => Err(ApiResponseError::API(errors)),
+        GetBalanceResponse::Response(vec) => Ok(vec),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::get_balance::get_balance;
+
+    #[tokio::test]
+    async fn get_balance_test() {
+        let balance = get_balance().await;
+        assert_eq!(balance.is_ok(), true)
+    }
+}
