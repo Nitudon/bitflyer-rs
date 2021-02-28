c5257f3c9ecebe76b4d70b665c6823c4c08bdc54
udon <myudongen@gmail.com>
2021-02-28 21:59:06 +0900

    collateral: 証拠金の情報取得APIの実装

---
+++ src/api/get_collateral.rs
@@ -0,0 +1,44 @@
+use crate::api;
+use crate::api::ApiResponseError;
+
+const PATH : &'static str = "/v1/me/getcollateral";
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum GetCollateralResponse {
+    Error { errors: Vec<String> },
+    Response { collateral: f32, open_position_pnl: f32, require_collateral: f32, keep_rate: f32}
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CollateralInfo {
+    pub collateral: f32,
+    pub open_position_pnl: f32,
+    pub require_collateral: f32,
+    pub keep_rate: f32
+}
+
+pub async fn get_collateral() -> Result<CollateralInfo, ApiResponseError> {
+    let response = api::get::<GetCollateralResponse>(&PATH).await?;
+
+    match response {
+        GetCollateralResponse::Error { errors } => Err(ApiResponseError::API(errors)),
+        GetCollateralResponse::Response {collateral, open_position_pnl, require_collateral, keep_rate} => Ok(CollateralInfo{
+            collateral,
+            open_position_pnl,
+            require_collateral,
+            keep_rate
+        }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::api::get_collateral::get_collateral;
+
+    #[tokio::test]
+    async fn get_collateral_test() {
+        let response = get_collateral().await;
+        assert_eq!(response.is_ok(), true)
+    }
+}
